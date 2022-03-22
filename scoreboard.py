5d1446b7b2b4f23e2956e82e18d8bbe5089b5dea
Jakub Grim <grimjakub@gmail.com>
2022-03-22 13:23:03 +0100

    commit

--- scoreboard.py
+++ scoreboard.py
@@ -8,8 +8,13 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt","r") as file:
-            self.high_score = int(file.read())
+        try:
+            with open("data.txt","r") as file:
+                self.high_score = int(file.read())
+        except:
+            self.high_score =0
+            with open("data.txt", "w") as file:
+                file.write(str(self.high_score))
         self.color("white")
         self.penup()
         self.goto(0, 250)
